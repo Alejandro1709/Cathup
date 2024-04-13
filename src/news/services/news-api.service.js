ce7c1f2f15f02e78f2e3e2a7da8c48695d75c7ab
= <alejandro.lr.developer@hotmail.com>
2024-04-12 19:43:29 -0500

    Add: footer component

--- src/news/services/news-api.service.js
+++ src/news/services/news-api.service.js
@@ -1,10 +1,12 @@
 import axios from "axios";
+import {LogoApiService} from "@/shared/services/logo-api.service.js";
 
 const http = axios.create({
     baseURL: 'https://newsapi.org/v2'
 })
 export class NewsApiService {
     apiKey = '2c474ace8ad3413fb73b18534c6a057e'
+    logoApi = new LogoApiService()
 
     getSources() {
         return http.get(`/top-headlines/sources?&apiKey=${this.apiKey}`)
@@ -12,5 +14,9 @@ export class NewsApiService {
 
     getArticlesForSource(sourceId) {
         return http.get(`/top-headlines?sources=${sourceId}&apiKey=${this.apiKey}`)
+    }
+
+    getUrlToLogo(source) {
+        return this.logoApi.getUrlToLogo(source)
     }
 }
